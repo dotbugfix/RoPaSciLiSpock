c7b086745772ddf5ece472a71ead3a132c4f5b40
ajinkya <ajinkya.a.rajguru@gmail.com>
2014-03-30 05:56:12 +0530

    SLF4J Android

--- RPSLS/src/com/h2b2/ropascilispock/EMoveList.java
+++ RPSLS/src/com/h2b2/ropascilispock/EMoveList.java
@@ -1,7 +1,7 @@
 package com.h2b2.ropascilispock;
 
 public enum EMoveList {
-	ML_ROCK(0),ML_PAPER(1),ML_SCISSOR(2),ML_LIZARD(3),ML_SPOCK(4);
+	ML_ROCK(0),ML_PAPER(1),ML_SCISSOR(2),ML_LIZARD(3),ML_SPOCK(4),ML_NO_RESPONSE(6);
 	
 	public final int value;
 	private EMoveList(final int value) {
--- RPSLS/src/com/h2b2/ropascilispock/ScoreMasterImplTest.java
+++ RPSLS/src/com/h2b2/ropascilispock/ScoreMasterImplTest.java
@@ -1,10 +1,12 @@
 package com.h2b2.ropascilispock;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ScoreMasterImplTest {
 
@@ -28,7 +30,8 @@ public class ScoreMasterImplTest {
 		int [][] a={{0,-1,1},{1,0,-1},{-1,1,0}};
 		ScoreMasterImpl scoreMaster=new ScoreMasterImpl();
 		scoreMaster.init(a);
-		
+		Logger logger = LoggerFactory.getLogger(ScoreMasterImpl.class);
+	    logger.info("Hello World");
 		assertEquals("Error",-1, scoreMaster.calculateScores(EMoveList.ML_ROCK, EMoveList.ML_PAPER));
 		assertEquals("Error",1, scoreMaster.calculateScores(EMoveList.ML_SCISSOR, EMoveList.ML_PAPER));
 		assertEquals("Error",-1, scoreMaster.calculateScores(EMoveList.ML_SCISSOR, EMoveList.ML_ROCK));
